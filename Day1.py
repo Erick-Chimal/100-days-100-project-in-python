784e322e42b3845bb8043b41edd8ab1102fd7388
Erick Chimal <125524019+Erick-Chimal@users.noreply.github.com>
2023-03-02 17:31:09 -0700

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,18 @@
+# Variables 
+print(len(input("What is your name? ")))
+name = input("What is your name? ")
+print(name + "!")
+a = input("a: ")
+b = input("b: ")
+c = a 
+a = b 
+b = c 
+print("a: " + a)
+print("b: " + b)
+
+# Simple Username Generator - project 1
+
+print("Simple Username Generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your name username could be {city} {pet}")
